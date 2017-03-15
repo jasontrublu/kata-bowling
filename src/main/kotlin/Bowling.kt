7c1cac461034c77ee5ca8b8682b2e557891979aa
Jason Peper <trublu@c-peper.de>
2017-03-15 21:37:32 +0000

    extract methods from roll

--- src/main/kotlin/Bowling.kt
+++ src/main/kotlin/Bowling.kt
@@ -8,21 +8,33 @@ class Bowling {
     }
 
     fun roll(value: Int): Unit {
+        handleStillActiveFrames(value)
+        if (currentFrame.isNew) {
+            handleFirstRollOfFrame(value)
+        } else {
+            handleSecondRollOfFrame(value)
+        }
+    }
+
+    private fun handleStillActiveFrames(value: Int) {
         activeFrames.forEach { it.addRoll(value) }
         activeFrames.removeAll { it.isClosed }
-        if (currentFrame.isNew) {
-            frames.add(currentFrame)
-            currentFrame.addRoll(value)
-            if (currentFrame.isStrike) {
-                activeFrames.add(currentFrame)
-                currentFrame = Frame()
-            }
-        } else {
-            currentFrame.addRoll(value)
-            if (currentFrame.isSpare) {
-                activeFrames.add(currentFrame)
-            }
+    }
+
+    private fun handleFirstRollOfFrame(value: Int) {
+        frames.add(currentFrame)
+        currentFrame.addRoll(value)
+        if (currentFrame.isStrike) {
+            activeFrames.add(currentFrame)
             currentFrame = Frame()
         }
+    }
+
+    private fun handleSecondRollOfFrame(value: Int) {
+        currentFrame.addRoll(value)
+        if (currentFrame.isSpare) {
+            activeFrames.add(currentFrame)
+        }
+        currentFrame = Frame()
     }
 }
